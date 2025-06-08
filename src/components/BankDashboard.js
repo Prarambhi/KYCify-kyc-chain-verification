6ecd042731af8c9a5305465ff0fb252c6ecbe801
prarambhi-k <159796556+Prarambhi@users.noreply.github.com>
2025-06-08 23:09:52 +0530

    Update BankDashboard.js

--- src/components/BankDashboard.js
+++ src/components/BankDashboard.js
@@ -85,7 +85,7 @@ const theme = createTheme({
   }
 });
 
-const CONTRACT_ADDRESS = "0x902D8529aF70C4578e50146298f5B51D93a91388";
+const CONTRACT_ADDRESS = "SMART_CONTRACT_ADDRESS";
 
 function BankDashboard() {
   const { account } = useWallet();
